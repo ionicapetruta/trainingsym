375a7dbccc74eeb723c74967260305c0e5052e55
Ionica <petruta_ionica@yahoo.com>
2014-09-19 10:32:30 +0300

    first commit to symfony

---
+++ src/Acme/HelloBundle/Controller/DefaultController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Acme\HelloBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\BrowserKit\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends Controller
+{
+
+//    public function indexAction($first_name, $last_name, $color)
+//    {
+//        $first_name = strtoupper($first_name);
+//
+//        // Render this page using hello-view template and send the parameters to
+//        // the twig.
+//        return $this->render(
+//            '@AcmeHello/Default/hello-view.html.twig',
+//            array(
+//                'name' => $first_name,
+//                'last_name' => $last_name,
+//               'color' => $color,
+//            )
+//        );
+//    }
+    public function indexAction($name)
+    {
+        $response = $this->forward('AcmeHelloBundle:Hello:fancy', array(
+                'name' => $name,
+                'color' => 'green',
+            ));
+        // ... further modify the response or return it directly
+        return $response;
+    }
+    public function fancyAction($name, $color)
+    {
+        // ... create and return a Response object
+    }
+
+
+
+}
